37b35b7a2be703a3d296657717ec2ef64d8cb6fa
wldcmzy <wldcmzy@163.com>
2022-05-20 15:52:32 +0800

    复读判官插件, 不再只处理纯文本, 而是可以处理图片了

--- uppercaveman_ellis/plugins/Ellis_repeat_judger/__init__.py
+++ uppercaveman_ellis/plugins/Ellis_repeat_judger/__init__.py
@@ -2,7 +2,7 @@ from nonebot import on_message
 from nonebot.adapters.onebot.v11 import Event, Message
 from nonebot.log import logger
 from .privilege import Role_RepeatJudger
-from .source_data import repeat_interrupt, repeat_whisper
+from .source_data import repeat_interrupt, repeat_whisper, remove_image_num
 from ..global_args import Repeat_judger_args_dic
 
 E_messsage = {}
@@ -13,21 +13,21 @@ async def repeat_judger(event : Event) -> None:
     #获取基本信息
     useless, group_id, user_id = event.get_session_id().split('_')
     group_id, user_id = int(group_id), int(user_id)
-    logger.debug('>>>>>>>>>>>>>>>' + str(group_id) +'|'+ str(user_id))
-    plain_text = event.get_plaintext()
+    msg = str(event.get_message())
+    msg = await remove_image_num(msg)
 
     # 若消息集合中没有这个群的信息, 新建一个
     if group_id not in E_messsage: 
-        E_messsage[group_id] = {'text' : plain_text, 'times' : 0}
+        E_messsage[group_id] = {'text' : msg, 'times' : 0}
     
     # 若信息变换, 重置消息集合中对应的内容
-    if E_messsage[group_id]['text'] != plain_text:
+    if E_messsage[group_id]['text'] != msg:
         # 若此群符合发送wisper陈述条件,且信息变换时此群已经在复读, 发送wisper内容
         if group_id not in  Repeat_judger_args_dic['interrupt_group'] and group_id in Repeat_judger_args_dic['whisper_group']:
             if E_messsage[group_id]['times'] >= Repeat_judger_args_dic['threshold']:
                 relpy_text = await repeat_whisper(user_id, E_messsage[group_id]['times'])
                 await E_repeat_judger.send(relpy_text)
-        E_messsage[group_id] = {'text' : plain_text, 'times' : 0}
+        E_messsage[group_id] = {'text' : msg, 'times' : 0}
 
     E_messsage[group_id]['times'] += 1
 
--- uppercaveman_ellis/plugins/Ellis_repeat_judger/source_data.py
+++ uppercaveman_ellis/plugins/Ellis_repeat_judger/source_data.py
@@ -1,4 +1,13 @@
 from nonebot.adapters.onebot.v11 import Message
+import re
+
+# 网上抄了一个处理图片的函数
+# 利用正则匹配所有信息中的图片信息并去除url
+async def remove_image_num(msg):
+    pattern = re.compile('\[CQ:image,file=(\w|\d)*\.image,url=https://gchat.qpic.cn/(\w|\d|\/|-|_|\?|=|,)*\]')
+    result = pattern.sub(lambda match : match.group().split(',url=')[0] + r"]}", msg)
+    # 防止操作系统换行符不同导致的问题
+    return result.replace('\r\n', '').replace('\n', '')
 
 async def repeat_whisper(user_id : int, times : int) -> Message:
     '''user_id : 用户的QQ号 times : 已经复读的次数'''
