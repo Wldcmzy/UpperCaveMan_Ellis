5ff7134fe01f1161385a2e347ae4efa0c6d35ba1
wldcmzy <wldcmzy@163.com>
2022-05-20 14:05:26 +0800

    手动为天气查询模块(weather)增加了很烂的自然语言处理

--- uppercaveman_ellis/plugins/Ellis_weather/__init__.py
+++ uppercaveman_ellis/plugins/Ellis_weather/__init__.py
@@ -1,14 +1,22 @@
-from nonebot import on_command
+from nonebot import on_command, on_keyword
 from nonebot.adapters.onebot.v11 import Event, Message
 from nonebot.params import CommandArg
 from .source_data import get_weather
 from nonebot.log import logger
+from jieba import posseg
 
-E_weather = on_command('天气')
+E_weather = on_keyword({'天气', 'weather'})
 
 @E_weather.handle()
-async def send_weather(event : Event, args : Message = CommandArg()) -> None:
-    # plain_text = event.get_plaintext()
-    plain_text = args.extract_plain_text()
-    reply_text = await get_weather(plain_text)
+async def send_weather(event : Event) -> None:
+    plain_text = event.get_plaintext().replace('天气', '').replace(' ', '')
+
+    words, city = posseg.lcut(plain_text), None
+    for each in words:
+        if each.flag == 'ns':
+            city = each.word
+            break
+    
+    if city == None: city = plain_text
+    reply_text = await get_weather(city)
     await E_weather.finish(reply_text)
