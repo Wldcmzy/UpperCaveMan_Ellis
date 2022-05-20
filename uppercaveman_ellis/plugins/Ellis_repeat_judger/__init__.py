63fecde1b138563174109b41a1e181f062b03a98
wldcmzy <wldcmzy@163.com>
2022-05-20 15:25:55 +0800

    复读whisper功能某些人无法触发, 后台报错nonebot.adapters.onebot.v11.exception.ActionFailed 未知原因 在CQ码附近加了空格又好了

--- uppercaveman_ellis/plugins/Ellis_repeat_judger/__init__.py
+++ uppercaveman_ellis/plugins/Ellis_repeat_judger/__init__.py
@@ -1,5 +1,3 @@
-from pydoc import plain
-from tokenize import group
 from nonebot import on_message
 from nonebot.adapters.onebot.v11 import Event, Message
 from nonebot.log import logger
@@ -15,18 +13,20 @@ async def repeat_judger(event : Event) -> None:
     #获取基本信息
     useless, group_id, user_id = event.get_session_id().split('_')
     group_id, user_id = int(group_id), int(user_id)
+    logger.debug('>>>>>>>>>>>>>>>' + str(group_id) +'|'+ str(user_id))
     plain_text = event.get_plaintext()
 
     # 若消息集合中没有这个群的信息, 新建一个
     if group_id not in E_messsage: 
         E_messsage[group_id] = {'text' : plain_text, 'times' : 0}
     
-    # 若信息变换, 重置消息和中对应的内容
-    # 另外, 若该群不在interrupt集合中并在whisper集合中, 发送wisper内容
+    # 若信息变换, 重置消息集合中对应的内容
     if E_messsage[group_id]['text'] != plain_text:
+        # 若此群符合发送wisper陈述条件,且信息变换时此群已经在复读, 发送wisper内容
         if group_id not in  Repeat_judger_args_dic['interrupt_group'] and group_id in Repeat_judger_args_dic['whisper_group']:
             if E_messsage[group_id]['times'] >= Repeat_judger_args_dic['threshold']:
-                await E_repeat_judger.send(await repeat_whisper(user_id, E_messsage[group_id]['times']))
+                relpy_text = await repeat_whisper(user_id, E_messsage[group_id]['times'])
+                await E_repeat_judger.send(relpy_text)
         E_messsage[group_id] = {'text' : plain_text, 'times' : 0}
 
     E_messsage[group_id]['times'] += 1
@@ -34,7 +34,8 @@ async def repeat_judger(event : Event) -> None:
     # 若此群在interrupt集合中并被判定为复读, 发送interrupt内容打断复读 
     if group_id in Repeat_judger_args_dic['interrupt_group']:
         if E_messsage[group_id]['times'] >= Repeat_judger_args_dic['threshold']:
-            await E_repeat_judger.send(await repeat_interrupt(user_id))
+            reply_text = await repeat_interrupt(user_id)
+            await E_repeat_judger.send(reply_text)
             E_messsage[group_id]['times'] -= Repeat_judger_args_dic['threshold']
         
     
--- uppercaveman_ellis/plugins/Ellis_repeat_judger/source_data.py
+++ uppercaveman_ellis/plugins/Ellis_repeat_judger/source_data.py
@@ -1,10 +1,9 @@
 from nonebot.adapters.onebot.v11 import Message
 
-async def repeat_whisper(user_id, times) -> Message:
+async def repeat_whisper(user_id : int, times : int) -> Message:
     '''user_id : 用户的QQ号 times : 已经复读的次数'''
-    # return Message(f'[CQ:at,qq={user_id}]终结了长达{times}次的复读, 为人类文明的进步迈出了一小步!')
-    return Message(f'[CQ:at,qq={user_id}]终结了长达{times}次的复读, 为人类的进步迈出了一小步!')
+    return Message(f' [CQ:at,qq={user_id}] 终结了长达{times}次的复读, 为人类文明的进步迈出了一小步!')
 
 async def repeat_interrupt(user_id) -> Message:
     '''user_id : 用户的QQ号'''
-    return Message(f'有复读人! 打断,并打断[CQ:at,qq={user_id}]的腿杀鸡儆猴!')
+    return Message(f'有复读人! 打断,并打断 [CQ:at,qq={user_id}] 的腿杀鸡儆猴!')
