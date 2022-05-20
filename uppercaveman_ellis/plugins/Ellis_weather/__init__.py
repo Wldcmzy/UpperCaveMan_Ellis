40021518ce4fec50fdbfce2c42fa35f7302bfa63
wldcmzy <wldcmzy@163.com>
2022-05-20 14:26:14 +0800

    为智障自然语言处理做了一点优化(但还是智障),修改weather插件代码结构

--- uppercaveman_ellis/plugins/Ellis_weather/__init__.py
+++ uppercaveman_ellis/plugins/Ellis_weather/__init__.py
@@ -1,22 +1,15 @@
 from nonebot import on_command, on_keyword
 from nonebot.adapters.onebot.v11 import Event, Message
 from nonebot.params import CommandArg
-from .source_data import get_weather
+from .source_data import get_weather, find_city
 from nonebot.log import logger
-from jieba import posseg
+
 
 E_weather = on_keyword({'天气', 'weather'})
 
 @E_weather.handle()
 async def send_weather(event : Event) -> None:
-    plain_text = event.get_plaintext().replace('天气', '').replace(' ', '')
-
-    words, city = posseg.lcut(plain_text), None
-    for each in words:
-        if each.flag == 'ns':
-            city = each.word
-            break
-    
-    if city == None: city = plain_text
+    plain_text = event.get_plaintext()
+    city = await find_city(plain_text)
     reply_text = await get_weather(city)
     await E_weather.finish(reply_text)
--- uppercaveman_ellis/plugins/Ellis_weather/source_data.py
+++ uppercaveman_ellis/plugins/Ellis_weather/source_data.py
@@ -1,6 +1,8 @@
+from jieba import posseg
 import requests
 import json
 import time
+from nonebot.log import logger
 
 async def get_weather(pos : str) -> str:
     '''中国地区名'''
@@ -29,3 +31,27 @@ async def get_weather(pos : str) -> str:
     except Exception as e:
         ret = '呜呜呜~\n出错了:' + str(e) + f'\n中华万年历里有这个地方吗?\n地点:{pos}'
     return ret 
+
+
+async def find_city(text : str) -> str:
+    text = text.replace('天气', '').replace(' ', '')
+
+    words, city = posseg.lcut(text), None
+    for each in words:
+        # logger.debug(each.word + ' ' + each.flag)
+        if each.flag == 'ns':
+            city = each.word
+            break
+
+    # 分词有时不准确, 这部分代码可以再一定程度上稍微补救, 但是经不起折腾
+    if city == None: 
+        for each in words:
+            if len(each.word) <= 1: continue
+            tail = each.word[-1]
+            if tail in ['市', '区', '县']:
+                city = each.word
+                break       
+        
+    if city == None: city = text
+
+    return city
